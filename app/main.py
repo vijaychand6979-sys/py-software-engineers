2d78ff4112d9cda6284f20d3b40390fc73481578
Vijay Chand <vijaychand6979@googlemail.com>
2026-03-03 11:58:07 +0000

    added code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,80 @@
-# write your code here
+class SoftwareEngineer:
+
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skill(
+        self,
+        skill: str
+    ) -> None:
+        if skill not in self.skills:
+            self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        super().__init__(name)
+        self.learn_skill("JavaScript")
+        self.learn_skill("HTML")
+        self.learn_skill("CSS")
+
+    def create_awesome_web_page(
+        self
+    ) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        super().__init__(name)
+        self.learn_skill("Python")
+        self.learn_skill("SQL")
+        self.learn_skill("Django")
+
+    def create_powerful_api(
+        self
+    ) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        super().__init__(name)
+        self.learn_skill("Java")
+        self.learn_skill("Android studio")
+
+    def create_smooth_mobile_app(
+        self
+    ) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        super().__init__(name)
+
+    def create_web_application(
+        self
+    ) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
